7c8e5e4531026c047291353ee2f30be12bcb4e49
Reyu Hero <reyu443@gmail.com>
2022-02-20 03:32:23 +0430

    customers ui

--- packages/Webkul/Admin/src/Resources/views/customers/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/customers/index.blade.php
@@ -2,15 +2,24 @@
 
 @section('page_title')
     {{-- {{ __('admin::app.customer.title') }} --}}
-    customer
+    All customers
 @stop
 @section('title')
     {{-- {{ __('admin::app.customer.title') }} --}}
-    customer
+    All customers
 @stop
-
+@section('navbar-top')
+<div class="pill bg-light grid column content-center fs-s hidden h-25 shadow-sm text-secondary" style="padding: 15px 5px;">
+    <a href="#" class="px-10 p-5 pill lh-1 h-25 bg-primary btn btn-primary border-0">
+        Customers
+    </a>
+    <a class="px-10 p-5 pill lh-1 h-25 ">
+        Policies
+    </a>
+</div>
+@stop
 @section('content-wrapper')
-<div class="content full-pag">
+<div class="content full-page">
 this is a test
 </div>
 @stop
