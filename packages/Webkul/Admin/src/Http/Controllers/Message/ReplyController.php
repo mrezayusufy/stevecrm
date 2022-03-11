b3733d3aa1a9add0f6ffc7279b6675120bb21b74
Reyu Hero <reyu443@gmail.com>
2022-03-11 11:47:18 +0430

    updated the reply

--- packages/Webkul/Admin/src/Http/Controllers/Message/ReplyController.php
+++ packages/Webkul/Admin/src/Http/Controllers/Message/ReplyController.php
@@ -2,14 +2,27 @@
 namespace Webkul\Admin\Http\Controllers\Message;
 use Webkul\Admin\Http\Controllers\Controller;
 use XmlResponse\Facades\XmlFacade;
+use Webkul\Message\Repositories\MessageRepository;
 
+// reply controller
 class ReplyController extends Controller {
-    public function __construct(){}
+    protected $messageRepository;
+    public function __construct(MessageRepository $messageRepository){
+        $this->messageRepository = $messageRepository;
+    }
     public function index() {
         $number = $_POST["From"];
         $body = $_POST["Body"];
-        
-        return response("<Response><Message>Dear Customer $number, You said: $body</Message></Response>", 200)
-                  ->header('Content-Type', 'text/xml');
+        $this->messageRepository->create([
+            "to" => $number,
+            "body" => $body
+        ]);
+        return response(
+            "<Response>".
+                "<Message>".
+                    "Dear Customer $number, You said: $body".
+                "</Message>".
+            "</Response>", 
+            200)->header('Content-Type', 'text/xml');
     }
 }
