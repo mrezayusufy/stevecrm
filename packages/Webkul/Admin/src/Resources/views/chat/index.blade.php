ae88dfc604baef990110677d7585fa2535f0ef35
Reyu Hero <reyu443@gmail.com>
2022-04-08 05:18:11 +0430

    chat implemented

---
+++ packages/Webkul/Admin/src/Resources/views/chat/index.blade.php
@@ -0,0 +1,13 @@
+@extends('admin::layouts.master')
+
+@section('page_title')
+    Chat
+@stop
+@section('title')
+    Chat
+@stop
+@section('content-wrapper')
+<div class="content full-page adjacent-center">
+    <chat></chat>
+</div>
+@stop
